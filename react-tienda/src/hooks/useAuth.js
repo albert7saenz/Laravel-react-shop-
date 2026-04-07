f6190b4f545f485874395d8f6daeef532129a5c0
Alberto Saenz <albertw042z@gmail.com>
2026-04-06 21:40:47 -0600

    feat(auth): add admin validation logic

--- react-tienda/src/hooks/useAuth.js
+++ react-tienda/src/hooks/useAuth.js
@@ -70,14 +70,20 @@ export const useAuth = ({middleware, url}) => {
         if(middleware === 'guest' && url && user){
             navigate(url)
         }
+        if(middleware == 'guest' && user && user.admin){
+            navigate('/admin')
+        }
 
+        if(middleware === 'admin' && user && !user.admin){
+            navigate('/')
+        }
         if(middleware === 'auth' && error){
             navigate('/auth/login')
         }
 
     }, [user, error])
 
-    console.log(middleware)
+    console.log(user)
     return {
         login,
         registro,
